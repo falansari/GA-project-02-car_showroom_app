d2477fa550c804d385872f85b67b4b7def34a872
Ash Alansari <twoseasgamesstudio@gmail.com>
2026-01-11 18:39:00 +0300

    Created OptionCategory controller + service closes #43

---
+++ src/main/java/com/ga/showroom/controller/OptionCategoryController.java
@@ -0,0 +1,80 @@
+package com.ga.showroom.controller;
+
+import com.ga.showroom.model.OptionCategory;
+import com.ga.showroom.service.OptionCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/option-categories")
+public class OptionCategoryController {
+    private OptionCategoryService optionCategoryService;
+
+    /**
+     * Initialize Option Category's service
+     * @param optionCategoryService OptionCategoryService
+     */
+    @Autowired
+    public void setOptionCategoryService(OptionCategoryService optionCategoryService) { this.optionCategoryService = optionCategoryService; }
+
+    /**
+     * Get an option category by its ID
+     * @param optionCategoryId  Long
+     * @return OptionCategory
+     */
+    @GetMapping("/{optionCategoryId}")
+    public OptionCategory getOptionCategoryById(@PathVariable("optionCategoryId") Long optionCategoryId) {
+        return optionCategoryService.getOptionCategoryById(optionCategoryId);
+    }
+
+    /**
+     * Get an option category by its name
+     * @param optionCategoryName String
+     * @return OptionCategory
+     */
+    @GetMapping("/category")
+    public OptionCategory getOptionCategoryByName(@RequestBody String optionCategoryName) {
+        return optionCategoryService.getOptionCategoryByName(optionCategoryName);
+    }
+
+    /**
+     * Get a list of all option categories
+     * @return List of OptionCategory
+     */
+    @GetMapping("")
+    public List<OptionCategory> getAllOptionCategories() {
+        return optionCategoryService.getOptionCategories();
+    }
+
+    /**
+     * Create a new option category
+     * @param optionCategory OptionCategory
+     * @return OptionCategory
+     */
+    @PostMapping("")
+    public OptionCategory createOptionCategory(@RequestBody OptionCategory optionCategory) {
+        return optionCategoryService.createOptionCategory(optionCategory);
+    }
+
+    /**
+     * Update an option category. Allows partial update.
+     * @param optionCategoryId Long
+     * @param optionCategory OptionCategory
+     * @return OptionCategory
+     */
+    @PatchMapping("/{optionCategoryId}")
+    public OptionCategory updateOptionCategory(@PathVariable("optionCategoryId") Long optionCategoryId, @RequestBody OptionCategory optionCategory) {
+        return optionCategoryService.updateOptionCategory(optionCategoryId, optionCategory);
+    }
+
+    /**
+     * Delete an existing option category by its ID
+     * @param optionCategoryId Long
+     */
+    @DeleteMapping("{optionCategoryId}")
+    public void deleteOptionCategory(@PathVariable("optionCategoryId") Long optionCategoryId) {
+        optionCategoryService.deleteOptionCategory(optionCategoryId);
+    }
+}
---
+++ src/main/java/com/ga/showroom/service/OptionCategoryService.java
@@ -0,0 +1,128 @@
+package com.ga.showroom.service;
+
+import com.ga.showroom.exception.InformationExistException;
+import com.ga.showroom.exception.InformationNotFoundException;
+import com.ga.showroom.model.OptionCategory;
+import com.ga.showroom.model.User;
+import com.ga.showroom.repository.OptionCategoryRepository;
+import com.ga.showroom.security.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequestMapping(path = "api/option-categories")
+public class OptionCategoryService {
+    OptionCategoryRepository optionCategoryRepository;
+
+    @Autowired
+    public void setOptionCategoryRepository(OptionCategoryRepository optionCategoryRepository) {
+        this.optionCategoryRepository = optionCategoryRepository;
+    }
+
+    public static User getCurrentLoggedInUser() {
+        MyUserDetails userDetails = (MyUserDetails) Objects.requireNonNull(SecurityContextHolder.getContext().getAuthentication()).getPrincipal();
+
+        assert userDetails != null;
+        return userDetails.getUser();
+    }
+
+    /**
+     * Get an option category by its ID
+     * @param optionCategoryId Long
+     * @return OptionCategory
+     */
+    public OptionCategory getOptionCategoryById(Long optionCategoryId) {
+        return optionCategoryRepository.findById(optionCategoryId)
+                .orElseThrow(() -> new InformationNotFoundException("OptionCategory with id " + optionCategoryId + " not found"));
+    }
+
+    /**
+     * Get an option category by its name
+     * @param optionCategoryName String
+     * @return OptionCategory
+     */
+    public OptionCategory getOptionCategoryByName(String optionCategoryName) {
+        OptionCategory optionCategory = optionCategoryRepository.findByName(optionCategoryName);
+
+        if (optionCategory == null) throw new InformationNotFoundException("OptionCategory with name " + optionCategoryName + " not found");
+        return optionCategory;
+    }
+
+    /**
+     * Get a list of all option categories
+     * @return List of OptionCategory
+     */
+    public List<OptionCategory> getOptionCategories() {
+        return optionCategoryRepository.findAll();
+    }
+
+    /**
+     * Create a new option category. Only admin allowed.
+     * @param optionCategory OptionCategory
+     * @return OptionCategory
+     */
+    public OptionCategory createOptionCategory(OptionCategory optionCategory) {
+        System.out.println("User role: " + getCurrentLoggedInUser().getRole());
+        // TODO: admin only should be able to create a option category. add with role management issue.
+        /*if (!Objects.equals(getCurrentLoggedInUser().getRole(), "admin")) {
+            throw new AccessDeniedException("Only an admin can access OptionCategory");
+        }*/
+
+        OptionCategory existingOptionCategory = optionCategoryRepository.findByName(optionCategory.getName());
+
+        if (existingOptionCategory != null) throw new InformationExistException("Option Category with name " + optionCategory.getName()+ " already exists");
+
+        return optionCategoryRepository.save(optionCategory);
+    }
+
+    /**
+     * Update an existing option category. Partial update allowed. Admin only.
+     * @param optionCategoryId Long
+     * @param optionCategory OptionCategory
+     * @return OptionCategory
+     */
+    public OptionCategory updateOptionCategory(Long optionCategoryId, OptionCategory optionCategory) {
+        // TODO: admin only should be able to create a option category. add with role management issue.
+        /*if (!Objects.equals(getCurrentLoggedInUser().getRole(), "admin")) {
+            throw new AccessDeniedException("Only an admin can access OptionCategory");
+        }*/
+
+        OptionCategory storedOptionCategory = getOptionCategoryById(optionCategoryId);
+
+        if (storedOptionCategory == null) throw new InformationNotFoundException("OptionCategory with id " + optionCategoryId + " not found");
+
+        if (optionCategory.getName() != null) {
+            OptionCategory existingOptionCategory = optionCategoryRepository.findByName(optionCategory.getName());
+
+            if (existingOptionCategory != null) throw new InformationExistException("Option Category with name " + optionCategory.getName()+ " already exists");
+
+            storedOptionCategory.setName(optionCategory.getName());
+        }
+
+        return optionCategoryRepository.save(storedOptionCategory);
+    }
+
+    /**
+     * Delete an existing option category by its ID. Admin only.
+     * @param optionCategoryId Long
+     */
+    public void deleteOptionCategory(Long optionCategoryId) {
+        // TODO: admin only should be able to create a option category. add with role management issue.
+        /*if (!Objects.equals(getCurrentLoggedInUser().getRole(), "admin")) {
+            throw new AccessDeniedException("Only an admin can access OptionCategory");
+        }*/
+
+        OptionCategory storedOptionCategory = getOptionCategoryById(optionCategoryId);
+
+        if (storedOptionCategory == null) throw new InformationNotFoundException("OptionCategory with id " + optionCategoryId + " not found");
+
+        optionCategoryRepository.delete(storedOptionCategory);
+    }
+}
