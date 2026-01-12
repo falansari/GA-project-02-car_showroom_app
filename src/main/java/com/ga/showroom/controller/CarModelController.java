383bf0a4cf944a7e9cbf73bf967eeb5fda099f4a
Ash Alansari <twoseasgamesstudio@gmail.com>
2026-01-12 19:42:54 +0300

    Created CarModelController with all get type requests #40

---
+++ src/main/java/com/ga/showroom/controller/CarModelController.java
@@ -0,0 +1,102 @@
+package com.ga.showroom.controller;
+
+import com.ga.showroom.model.Car;
+import com.ga.showroom.model.CarModel;
+import com.ga.showroom.model.Option;
+import com.ga.showroom.service.CarModelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Year;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/car-models")
+public class CarModelController {
+    private CarModelService carModelService;
+
+    @Autowired
+    public void setCarModelService(CarModelService carModelService) {
+        this.carModelService = carModelService;
+    }
+
+    /**
+     * Find car model by its ID
+     * @param carModelId Long
+     * @return CarModel
+     */
+    @GetMapping("/{carModelId}")
+    public CarModel getCarModelById(@PathVariable("carModelId") long carModelId) {
+        return carModelService.getCarModelById(carModelId);
+    }
+
+    /**
+     * Find car model by its name
+     * @param carModelName String
+     * @return CarModel
+     */
+    @GetMapping("/car-model/{carModelName}")
+    public CarModel getCarModelByName(@PathVariable("carModelName") String carModelName) {
+        return carModelService.getCarModelByName(carModelName);
+    }
+
+    /**
+     * Get all car models list
+     * @return List of CarModel
+     */
+    @GetMapping("")
+    public List<CarModel> getAllCarModels() {
+        return carModelService.getAllCarModels();
+    }
+
+    /**
+     * Get all car models by their make year
+     * @param year Year
+     * @return List of CarModel
+     */
+    @GetMapping("/{year}")
+    public List<CarModel> getAllCarModelsByYear(@PathVariable("year") Year year) {
+        return carModelService.getAllByMakeYear(year);
+    }
+
+    /**
+     * Get all car models with make year between start and end year.
+     * @param startYear Year
+     * @param endYear Year
+     * @return List of CarModel
+     */
+    @GetMapping("/years")
+    public List<CarModel> getAllCarModelsByYearBetween(@RequestParam Year startYear, @RequestParam Year endYear) {
+        return carModelService.getAllByMakeYearBetween(startYear, endYear);
+    }
+
+    /**
+     * Get all car models of a specific manufacturer
+     * @param manufacturer String
+     * @return List of CarModel
+     */
+    @GetMapping("/manufacturer")
+    public List<CarModel> getAllCarModelsByManufacturer(@RequestParam String manufacturer) {
+        return carModelService.getAllByManufacturer(manufacturer);
+    }
+
+    /**
+     * Get all car options available to a specific car model
+     * @param carModelId Long
+     * @return List of Option
+     */
+    @GetMapping("/{carModelId}/options")
+    public List<Option> getAllOptionsByCarModelId(@PathVariable("carModelId") long carModelId) {
+        return carModelService.getAllOptionsByCarModelId(carModelId);
+    }
+
+    /**
+     * Get all cars of a specific car model
+     * @param carModelId Long
+     * @return List of Car
+     */
+    @GetMapping("/{carModelId}/cars")
+    public List<Car> getAllCarsByCarModelId(@PathVariable("carModelId") long carModelId) {
+        return carModelService.getAllCarsByCarModelId(carModelId);
+    }
+}
